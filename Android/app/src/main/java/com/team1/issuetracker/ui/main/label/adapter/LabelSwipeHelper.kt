189077a7bedaddc5dfef47a31319da5d31885fe5
ese111 <feung94@gmail.com>
2022-06-24 01:12:43 +0900

    feat: swipeHelper 수정

--- Android/app/src/main/java/com/team1/issuetracker/ui/main/label/adapter/LabelSwipeHelper.kt
+++ Android/app/src/main/java/com/team1/issuetracker/ui/main/label/adapter/LabelSwipeHelper.kt
@@ -5,20 +5,21 @@ import androidx.recyclerview.widget.RecyclerView
 import com.team1.issuetracker.R
 import com.team1.issuetracker.common.SwipeHelper
 import com.team1.issuetracker.ui.main.label.adapter.LabelAdapter
+import com.team1.issuetracker.ui.main.milestone.adapter.MilestoneAdapter
 
 class LabelSwipeHelper: SwipeHelper() {
 
     override fun getView(viewHolder: RecyclerView.ViewHolder): View {
-        return (viewHolder as LabelAdapter.LabelViewHolder).itemView.findViewById(R.id.label_view) // 아이템뷰에서 스와이프 영역에 해당하는 뷰 가져오기
+        return (viewHolder as MilestoneAdapter.MilestoneViewHolder).itemView.findViewById(R.id.milestone_view) // 아이템뷰에서 스와이프 영역에 해당하는 뷰 가져오기
     }
 
     override fun setClamped(viewHolder: RecyclerView.ViewHolder, isClamped: Boolean){
-        viewHolder as LabelAdapter.LabelViewHolder
+        viewHolder as MilestoneAdapter.MilestoneViewHolder
         viewHolder.setClamped(isClamped)
     }
 
     override fun getClamped(viewHolder: RecyclerView.ViewHolder): Boolean{
-        viewHolder as LabelAdapter.LabelViewHolder
+        viewHolder as MilestoneAdapter.MilestoneViewHolder
         return viewHolder.getClamped()
     }
 }
